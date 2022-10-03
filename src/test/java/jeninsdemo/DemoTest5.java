c2df3f10d0ed08388f01332b0c0a91e73312f560
Ruthuparna <ruthu199425@gmail.com>
2022-10-03 06:57:25 +0530

    commit demo

---
+++ src/test/java/jeninsdemo/DemoTest5.java
@@ -0,0 +1,12 @@
+package jeninsdemo;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class DemoTest5 {
+	@Test
+	public void tc3()
+	{
+		Reporter.log("Helo python", true);
+	}
+}
